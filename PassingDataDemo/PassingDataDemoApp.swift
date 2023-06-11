2fcf7149e8fcb36a3e790cfed6546ccd4b66701f
Syed Raza <syed.raza011@gmail.com>
2023-06-11 13:14:03 -0400

    Initial Commit

---
+++ PassingDataDemo/PassingDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataDemoApp.swift
+//  PassingDataDemo
+//
+//  Created by Syed Raza on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PassingDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
